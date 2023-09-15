59d9f84da2b43f6c89af803bfb17672c75489edf
manuvillega <manueldxt@gmail.com>
2023-09-15 16:46:29 -0300

    Update Vistas.java

--- src/universidadgrupo8/vistas/Vistas.java
+++ src/universidadgrupo8/vistas/Vistas.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package universidadgrupo8.vistas;
 
-/**
- *
- * @author manue
- */
+
 public class Vistas {
+    private String hola;
+    
     
 }
