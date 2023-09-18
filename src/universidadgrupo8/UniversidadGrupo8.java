3a5777931fa75671731d1d814a28542fbb4bbc6a
Gonza <103911012+D3Po1@users.noreply.github.com>
2023-09-18 01:12:52 -0300

    Resolviendo conflictos Git

--- src/universidadgrupo8/UniversidadGrupo8.java
+++ src/universidadgrupo8/UniversidadGrupo8.java
@@ -1,16 +1,9 @@
 package universidadgrupo8;
 
-//import universidadgrupo8.accesoADatos.MateriaData;
-///import universidadgrupo8.entidades.Materia;
-
 public class UniversidadGrupo8 {
 
     public static void main(String[] args) {
 
-        /*Materia mat = new Materia("Analisis", 1, true);
-        MateriaData m = new MateriaData();
-        m.guardarMateria(mat);*/
-        
     }
 
 }
